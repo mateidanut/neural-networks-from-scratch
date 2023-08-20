16174c93fb25badd7b5931d5d5900a9a6e90d0a6
Gabriel-Danut Matei <matei.danut.dm@gmail.com>
2023-08-20 13:03:56 +0300

    Now layer works with batched input

--- layer.py
+++ layer.py
@@ -6,14 +6,31 @@ class Layer:
         self.input_size = input_size
         self.output_size = output_size
 
-        self.neurons = np.random.randn(input_size + 1, output_size)
+        #self.neurons = np.random.randn(input_size + 1, output_size)
+        self.neurons = np.ones((input_size + 1, output_size))
 
 
-    def __call__(self, v_input):
-        return np.append(1, v_input) @ self.neurons
+    def __call__(self, batched_input):
+
+        batched_input_with_bias = np.insert(
+            batched_input,
+            0,
+            1,
+            axis=1
+        )
+
+        print('INITIAL INPUT SHAPE', batched_input.shape)
+        print('INPUT SHAPE', batched_input_with_bias.shape)
+        print('LAYER SHAPE', self.neurons.shape)
+
+        return batched_input_with_bias @ self.neurons
 
 
 if __name__ == '__main__':
     layer = Layer(3, 4)
 
-    print(layer(np.array([1, 2, 3])))
+    print(layer(np.array([
+            [1, 2, 3],
+            [1, 1, 1],
+        ])
+        ))
